7749bb4ba40d429d1d0170fa062ec45e47a7c484
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-07-28 16:31:51 +0500

    Оператор switch

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main {
+/*public class Main {
    
    public static void main(String[] args) {
       
@@ -13,5 +13,33 @@ public class Main {
       else {
          System.out.println("Вам нет 18. Пока");
       }
+   }
+}*/
+
+public class Main {
+   
+   public static void main (String[] args) {
+      
+      String day = "Вторник";
+      
+      switch (day) {
+         case "Понедельник": System.out.println("Сегодня понедельник");
+         break;
+         case "Вторник": System.out.println("Сегодня вторник");
+         break;
+         case "Среда": System.out.println("Сегодня среда");
+         break;
+         case "Четверг": System.out.println("Сегодня четверг");
+         break;
+         case "Пятница": System.out.println("Сегодня пятница");
+         break;
+         case "Суббота": System.out.println("Сегодня суббота");
+         break;
+         case "Воскресенье": System.out.println("Сегодня воскресенье");
+         break;
+         default: System.out.println("Нет совпадений");
+      }
+      
+      
    }
 }
