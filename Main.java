50fb149f1ab18900450548903ce5e1eb17529d5b
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-08-03 21:37:16 +0500

    Оператор ветвления if

--- Main.java
+++ Main.java
@@ -66,3 +66,19 @@ public static void main (String[] args) {
 
 import java.util.Scanner;
 
+public class Main {
+   public static void main(String[] args) {
+      Scanner in = new Scanner (System.in);
+      int x = 0;
+      System.out.println("Введите целое число: ");
+      x = in.nextInt();
+      if (x % 2 == 0)
+         System.out.println(x + "- четное число");
+      else
+         System.out.println(x + "- нечетное число");
+      System.out.println();
+         
+      
+   }
+}
+
