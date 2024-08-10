2be73694482fde26594f6eb29a35069e05b0878e
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-08-10 16:53:49 +0500

    Изменение порядка следования элементов массиве на противоположный

--- Main.java
+++ Main.java
@@ -461,5 +461,28 @@ public class Main {
          if (is_swap == false) break;
       }
       
-   }
+   } 
 }*/
+
+import java.util.Arrays;
+
+public class Main {
+   public static void main (String[] args) {
+      int [] arr = {1, 2, 3, 4, 5};
+      reverse (arr);
+      System.out.println(Arrays.toString(arr));
+      reverse (arr);
+      System.out.println(Arrays.toString(arr));
+      }
+      public static void reverse (int[] arr) {
+         int tmp = 0;
+         for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            tmp = arr [j];
+            arr[i] = arr [j];
+            arr[j] = tmp;
+         
+      }
+      
+   }
+}
+
