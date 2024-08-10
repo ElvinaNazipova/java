77cc41e30bfa4f4add845707663df99799f85730
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-08-10 16:21:05 +0500

    Сортировка только часть массива

--- Main.java
+++ Main.java
@@ -402,10 +402,23 @@ public class Main {
    }*/
 
 
-import java.util.Arrays;
+/*import java.util.Arrays;
+public class Main{
 public static void main (String [] args) {
    int [] arr = {10,5,6,1,3};
    System.out.println(Arrays.toString(arr));
    Arrays.sort(arr);
    System.out.println(Arrays.toString(arr));
  }
+}
+*/
+
+import java.util.Arrays;
+
+public class Main {
+   public static void main (String[] args) {
+      int [] arr = {10,5,6,1,3};
+      Arrays.sort(arr, 0, 3);
+      System.out.println(Arrays.toString(arr));
+   }
+}
