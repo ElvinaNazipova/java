291e574faf2fab1118227a74b9e09f1368d78df5
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-08-10 16:42:09 +0500

    Пузырьковая сортировка по возрастанию

--- Main.java
+++ Main.java
@@ -424,7 +424,7 @@ public class Main {
 }
 */
 
-import java.util.Arrays;
+/*import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
@@ -434,3 +434,32 @@ public class Main {
       System.out.println(Arrays.toString(arr));
    }
 }
+*/
+
+
+/*import java.util.Arrays;
+
+public class Main {
+   public static void main (String[] args) {
+      int [] arr = {10,5,6,1,3};
+      sort (arr);
+      System.out.println(Arrays.toString(arr));
+   }
+   public static void sort (int[] arr) {
+      int tmp = 0, k = arr.length - 2;
+      boolean is_swap = false;
+      for (int i = 0; i <= k; i++) {
+         is_swap = false;
+         for (int j = k; j >=i; j--) {
+            if (arr[j] > arr[j +1]) {
+               tmp = arr [j + 1];
+               arr [j + 1] = arr [j];
+               arr [j] = tmp;
+               is_swap = true;
+            }
+         }
+         if (is_swap == false) break;
+      }
+      
+   }
+}*/
