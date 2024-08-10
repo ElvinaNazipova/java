f41f6a0de0c880a94cc4faf10e6afbc6b7ed3613
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-08-10 16:23:01 +0500

    Сортировка массива  обратном порядке

--- Main.java
+++ Main.java
@@ -413,12 +413,24 @@ public static void main (String [] args) {
 }
 */
 
-import java.util.Arrays;
+/*import java.util.Arrays;
 
 public class Main {
    public static void main (String[] args) {
       int [] arr = {10,5,6,1,3};
       Arrays.sort(arr, 0, 3);
+      System.out.println(Arrays.toString(arr));
+   }
+}
+*/
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+   public static void main (String[] args) {
+      Integer[] arr = {10,5,6,1,3};
+      Arrays.sort(arr, Collections.reverseOrder());
       System.out.println(Arrays.toString(arr));
    }
 }
