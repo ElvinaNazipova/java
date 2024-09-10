2fba257e959b25f36350df76a9289206d636312e
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-09-10 22:25:12 +0500

    Справочная

--- Main.java
+++ Main.java
@@ -587,7 +587,7 @@ public class Main {
    }
 }*/
 
-public class Main {
+/*public class Main {
    public static void main (String [] args) {
       int p,q;
       System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
@@ -611,6 +611,41 @@ public class Main {
       System.out.print(p + "\t" + q + "\t");
       System.out.print((p&q)+"\t" + (p|q)+"\t");
       System.out.println((p^q) + "\t" + (1-p));
+      
+   }
+}*/
+
+public class Main {
+   public static void main (String [] args) 
+      throws java.io.IOException {
+         char choice;
+         
+      System.out.println("Справка по: ");
+      System.out.println("1. if");
+      System.out.println("2. switch");
+      System.out.println("Выберите вариант: ");
+      
+      choice = (char) System.in.read();
+      
+      switch (choice) {
+      case '1':
+         System.out.println("Оператор if:\n");
+         System.out.println("if (условие) оператор;");
+         System.out.println("else оператор");
+         break;
+      case '2':
+         System.out.println("Традиционный оператор switch:\n");
+         System.out.println("switch (выражение) {");
+         System.out.println("   case констатнта:");
+         System.out.println("   последовательность операторов");
+         System.out.println("   break");
+         System.out.println("}");
+         break;
+         default:
+         System.out.println("Выбранный вариант не найден.");
+         
+      }
+      
       
    }
 }
