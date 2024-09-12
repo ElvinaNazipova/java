a422f433d0644ff26f7a7d8ca41a779710aa68f0
Elvina Nazipova <elvina.valeeva.2002@bk.ru>
2024-09-12 19:46:44 +0500

    Новый файл для практики по книге

--- Main.java
+++ Main.java
@@ -614,38 +614,5 @@ public class Main {
       
    }
 }*/
+    
 
-public class Main {
-   public static void main (String [] args) 
-      throws java.io.IOException {
-         char choice;
-         
-      System.out.println("Справка по: ");
-      System.out.println("1. if");
-      System.out.println("2. switch");
-      System.out.println("Выберите вариант: ");
-      
-      choice = (char) System.in.read();
-      
-      switch (choice) {
-      case '1':
-         System.out.println("Оператор if:\n");
-         System.out.println("if (условие) оператор;");
-         System.out.println("else оператор");
-         break;
-      case '2':
-         System.out.println("Традиционный оператор switch:\n");
-         System.out.println("switch (выражение) {");
-         System.out.println("   case констатнта:");
-         System.out.println("   последовательность операторов");
-         System.out.println("   break");
-         System.out.println("}");
-         break;
-         default:
-         System.out.println("Выбранный вариант не найден.");
-         
-      }
-      
-      
-   }
-}
---
+++ Practice.txt
@@ -0,0 +1,8 @@
+public class Practice {
+public static void Main (String [] args) {
+int x = 0;
+int y = 8;
+int sum = x + y;
+Sysyem.out.println (sum);
+}
+}
